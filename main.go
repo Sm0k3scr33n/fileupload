7ef8624044759122709f01775401dd99387530a0
Ben <ben.gabbard@live.com>
2016-10-21 10:01:59 -0600

    first api commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	
+	"log"
+	"net/http"
+	
+)
+
+// 1MB
+const MAX_MEMORY = 1 * 1024 * 1024
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(MAX_MEMORY); err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusForbidden)
+	}
+
+	for key, value := range r.MultipartForm.Value {
+		fmt.Fprintf(w, "%s:%s ", key, value)
+		log.Printf("%s:%s", key, value)
+	}
+
+
+}
+
+func main() {
+	http.HandleFunc("/upload", upload)
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
